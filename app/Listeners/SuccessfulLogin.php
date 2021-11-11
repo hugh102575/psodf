71ef0850e2b1f1b3812644b5045e3cfc054dcac7
root <root@yes-patent.yesinfo.com.tw>
2021-11-11 16:58:17 +0800

    2021_11_11 server sign msg deault value problem

--- app/Listeners/SuccessfulLogin.php
+++ app/Listeners/SuccessfulLogin.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Auth;
 use Illuminate\Support\Str;
+use App\Models\User;
 class SuccessfulLogin
 {
     /**
@@ -26,10 +27,25 @@ class SuccessfulLogin
      */
     public function handle($event)
     {
-        $user = Auth::user();
+        /*$user = Auth::user();
         if($user->api_token==null){
             $user->api_token = Str::random(60);
             $user->save();
+        }*/
+        $user = Auth::user();
+        if($user->api_token==null){
+            $api_token=Str::random(60);
+            while(User::where('api_token',$api_token)->exists()) {
+                $api_token=Str::random(60);
+            }
+            $user->api_token = $api_token;
+            $user->save();
+        }
+        $school = Auth::user()->school;
+        if($school->in_msg==null || $school->out_msg==null){
+            $school->in_msg="您的孩子@Name已經到班囉!";
+            $school->out_msg="您的孩子@Name已經下課囉!";
+            $school->save();
         }
     }
 }
--- database/migrations/2021_11_09_134405_add_sign_msg_to_school.php
+++ database/migrations/2021_11_09_134405_add_sign_msg_to_school.php
@@ -14,8 +14,10 @@ class AddSignMsgToSchool extends Migration
     public function up()
     {
         Schema::table('school', function (Blueprint $table) {
-            $table->text('in_msg')->after('sign_mode')->default('您的孩子@Name已經到班囉!')->nullable();
-            $table->text('out_msg')->after('in_msg')->default('您的孩子@Name已經下課囉!')->nullable();
+            //$table->text('in_msg')->after('sign_mode')->default('您的孩子@Name已經到班囉!')->nullable();
+            //$table->text('out_msg')->after('in_msg')->default('您的孩子@Name已經下課囉!')->nullable();
+            $table->text('in_msg')->after('sign_mode')->nullable();
+            $table->text('out_msg')->after('in_msg')->nullable();
         });
     }
 
