8ff1c386417acf6a64be947fb736b07399fe10a0
root <root@yes-patent.yesinfo.com.tw>
2022-03-07 15:30:02 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -51,7 +51,7 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
+    {   //$test=123;
         $now = date('Y-m-d H:i:s');
         return view('index',['now'=>$now]);
         //return view('index');
